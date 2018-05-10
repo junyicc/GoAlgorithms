c116392097f1b621dacdc7275009f1f5f964cb24
CAIJUNYI <cai_junyi@yeah.net>
2018-05-10 11:17:28 +0800

    implementation of bit operation

---
+++ interviews/10-bits.go
@@ -0,0 +1,33 @@
+package interviews
+
+// NumOf1 returns number of 1 in the bits of n
+func NumOf1(n int) int {
+	cnt := 0
+	for n != 0 {
+		n = n & (n - 1)
+		cnt++
+	}
+	return cnt
+}
+
+// IsPowerOf2 return true if n is the power of 2
+func IsPowerOf2(n int) bool {
+	if n > 0 {
+		return n&(n-1) == 0
+	}
+	return false
+}
+
+// NumOfChangingBits returns number of bits that are needed to be changed from m to n
+func NumOfChangingBits(m, n int) int {
+	diff := m ^ n
+	return NumOf1(diff)
+}
+
+// IsOdd return true if n is an odd number
+func IsOdd(n uint) bool {
+	if n == 0 {
+		return false
+	}
+	return n&1 == 1
+}
