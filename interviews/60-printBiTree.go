bcf23ad251e7d99dae005d465760b572a1bb8a92
CAIJUNYI <cai_junyi@yeah.net>
2018-06-02 12:17:04 +0800

    implementation of printing binary tree

---
+++ interviews/60-printBiTree.go
@@ -0,0 +1,36 @@
+package interviews
+
+import (
+	"fmt"
+
+	"github.com/CAIJUNYI/GoAlgorithms/datastructure"
+)
+
+// PrintBinaryTree prints nodes multiline
+func PrintBinaryTree(root *datastructure.TreeNode) {
+	queue := new(datastructure.DynamicQueue)
+	queue = queue.New()
+	queue.Enqueue(root)
+	cnt := 0
+	toBePrinted := 1
+	for !queue.IsEmpty() {
+		node := (*queue.Dequeue()).(*datastructure.TreeNode)
+		fmt.Printf("%v\t", node.Data)
+		toBePrinted--
+
+		if node.LChild != nil {
+			queue.Enqueue(node.LChild)
+			cnt++
+		}
+		if node.RChild != nil {
+			queue.Enqueue(node.RChild)
+			cnt++
+		}
+
+		if toBePrinted == 0 {
+			fmt.Println()
+			toBePrinted = cnt
+			cnt = 0
+		}
+	}
+}
--- test/interviews_test.go
+++ test/interviews_test.go
@@ -1344,3 +1344,15 @@ func Test59IsSymmertrical(t *testing.T) {
 		t.Errorf("expected true and got %t", b)
 	}
 }
+
+func Test60PrintBiTree(t *testing.T) {
+	n5L := datastructure.TreeNode{Data: 5}
+	n7L := datastructure.TreeNode{Data: 7}
+	n6L := datastructure.TreeNode{Data: 6, LChild: &n5L, RChild: &n7L}
+	n5R := datastructure.TreeNode{Data: 5}
+	n7R := datastructure.TreeNode{Data: 7}
+	n6R := datastructure.TreeNode{Data: 6, LChild: &n7R, RChild: &n5R}
+	n8 := datastructure.TreeNode{Data: 8, LChild: &n6L, RChild: &n6R}
+
+	interviews.PrintBinaryTree(&n8)
+}
