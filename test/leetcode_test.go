47c32d14a3b4a38d68c1679e6e90774406564ef0
CAIJUNYI <cai_junyi@yeah.net>
2018-07-01 10:20:25 +0800

    three sum problems

---
+++ test/leetcode_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/CAIJUNYI/GoAlgorithms/leetcode"
+)
+
+func TestThreeSum(t *testing.T) {
+	result := leetcode.ThreeSum([]int{-1, 0, 1, 2, -1, -4})
+	fmt.Println(result)
+
+	result = leetcode.ThreeSum([]int{0, 0, 0})
+	fmt.Println(result)
+}
+
+func TestThreeSumClosest(t *testing.T) {
+	if result := leetcode.ThreeSumClosest([]int{-1, 2, 1, 4}, 1); result != 2 {
+		t.Errorf("expected 2 and got %d", result)
+	}
+}
