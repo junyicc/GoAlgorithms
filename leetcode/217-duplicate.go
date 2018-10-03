c72ecbaa011062fdac76595e63f9b9be647ea84a
CAIJUNYI <cai_junyi@yeah.net>
2018-10-03 15:01:23 +0800

    leetcode 217: contains duplicate

--- leetcode/217-duplicate.go
+++ leetcode/217-duplicate.go
@@ -1,6 +1,9 @@
 package leetcode
 
 func containsDuplicate(nums []int) bool {
+	if nums == nil || len(nums) < 2 {
+		return false
+	}
 	n := len(nums)
 	if nums == nil || n < 1 {
 		return false
@@ -13,4 +16,34 @@ func containsDuplicate(nums []int) bool {
 		numsMap[num] = true
 	}
 	return false
+}
+
+func containsDuplicateII(nums []int) bool {
+	if nums == nil || len(nums) < 2 {
+		return false
+	}
+	return partition(nums, 0, len(nums)-1)
+}
+
+func partition(nums []int, lo, hi int) bool {
+	if hi <= lo {
+		return false
+	}
+	val := nums[(lo+hi)>>1]
+	left, right := lo, hi
+	for left <= right {
+		for left <= right && nums[left] < val {
+			left++
+		}
+		for left <= right && nums[right] > val {
+			right--
+		}
+		if left <= right {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+			right--
+		}
+	}
+	return right >= lo && left <= hi && nums[left] == nums[right] ||
+		partition(nums, lo, right) || partition(nums, left, hi)
 }
