9e06a60623dc9486f4f544c15bbb4c3c4a4c19aa
CAIJUNYI <cai_junyi@yeah.net>
2018-10-04 17:10:44 +0800

    leetcode 141: linkedlist cycle

---
+++ leetcode/141-linkedlistCycle.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
