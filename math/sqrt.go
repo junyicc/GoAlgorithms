54f9e46a3858f25769051390029a892e005b1b03
CAIJUNYI <cai_junyi@yeah.net>
2018-10-05 17:01:49 +0800

    leetcode 69: sqrt

---
+++ math/sqrt.go
@@ -0,0 +1,14 @@
+package math
+
+// Newton's method (https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division)
+
+func intSqrt(x int) int {
+	if x <= 0 {
+		return 0
+	}
+	r := x
+	for r*r > x {
+		r = (r + x/r) / 2
+	}
+	return r
+}
