13ecc0bda34de7adecfd571f36a11dd093d2e285
junyi <cai_junyi@yeah.net>
2020-06-08 14:16:16 +0800

    0-1 knapsack with value

--- dynamic_programming/knapsack.go
+++ dynamic_programming/knapsack.go
@@ -36,3 +36,44 @@ func KnapsackWithWeight(items []int, w int) int {
 	}
 	return 0
 }
+
+func KnapsackWithValue(itemWeight []int, itemValue []int, w int) int {
+	if len(itemWeight) < 1 || len(itemValue) < 1 || len(itemWeight) != len(itemValue) {
+		return 0
+	}
+	n := len(itemWeight)
+
+	states := make([][]int, n)
+	for i := 0; i < n; i++ {
+		states[i] = make([]int, w+1)
+	}
+
+	// init state
+	states[0][itemWeight[0]] = itemValue[0]
+
+	for i := 1; i < n; i++ {
+		// do no put item i into knapsack
+		for j := 0; j < w+1; j++ {
+			if states[i-1][j] > 0 {
+				states[i][j] = states[i-1][j]
+			}
+		}
+
+		// put item i into knapsack
+		for j := 0; j < w+1-itemWeight[i]; j++ {
+			if states[i-1][j] > 0 {
+				v := states[i-1][j] + itemValue[i]
+				if v > states[i][j+itemWeight[i]] {
+					states[i][j+itemWeight[i]] = v
+				}
+			}
+		}
+	}
+	maxValue := 0
+	for j := w; j >= 0; j-- {
+		if states[n-1][j] > maxValue {
+			maxValue = states[n-1][j]
+		}
+	}
+	return maxValue
+}
--- dynamic_programming/knapsack_test.go
+++ dynamic_programming/knapsack_test.go
@@ -13,7 +13,7 @@ func TestKnapsackWithWeight(t *testing.T) {
 		want int
 	}{
 		{
-			name: "0-1 knapsack",
+			name: "0-1 knapsack with weight",
 			args: args{
 				items: []int{2, 2, 4, 6, 3},
 				w:     9,
@@ -25,6 +25,36 @@ func TestKnapsackWithWeight(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := KnapsackWithWeight(tt.args.items, tt.args.w); got != tt.want {
 				t.Errorf("KnapsackWithWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKnapsackWithValue(t *testing.T) {
+	type args struct {
+		itemWeight []int
+		itemValue  []int
+		w          int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "0-1 knapsack with value",
+			args: args{
+				itemWeight: []int{2, 2, 4, 6, 3},
+				itemValue:  []int{3, 4, 8, 9, 6},
+				w:          9,
+			},
+			want: 17,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := KnapsackWithValue(tt.args.itemWeight, tt.args.itemValue, tt.args.w); got != tt.want {
+				t.Errorf("KnapsackWithValue() = %v, want %v", got, tt.want)
 			}
 		})
 	}
