6efc065ee1ae139b085e0f19dbb4898368e3a931
junyi <cai_junyi@yeah.net>
2020-06-15 20:57:25 +0800

    leetcode 70: climb stairs

--- leetcode/70-climbStairs.go
+++ leetcode/70-climbStairs.go
@@ -10,14 +10,13 @@ func climbStairs(n int) int {
 	if n <= 0 {
 		return 0
 	}
+
 	if n == 1 {
 		return 1
 	}
-	if n == 2 {
-		return 2
-	}
-	a, b := 1, 2
-	for i := 2; i < n; i++ {
+
+	a, b := 1, 1
+	for i := 1; i < n; i++ {
 		a, b = b, a+b
 	}
 	return b
