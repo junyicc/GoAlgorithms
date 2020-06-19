9e048988f6ecd84422f19df9ac2c461446313aec
junyi <cai_junyi@yeah.net>
2020-06-19 23:31:15 +0800

    first missing positive testing

--- leetcode/41-firstMissingPositive.go
+++ leetcode/41-firstMissingPositive.go
@@ -14,11 +14,58 @@ package leetcode
 //
 // Input: [7,8,9,11,12]
 // Output: 1
+
 // Note:
-//
 // Your algorithm should run in O(n) time and uses constant extra space.
+
 func firstMissingPositive(nums []int) int {
 	if len(nums) < 1 {
 		return 0
 	}
+
+	// check min positive 1 exists
+	exists1 := false
+	for _, num := range nums {
+		if num == 1 {
+			exists1 = true
+			break
+		}
+	}
+	if !exists1 {
+		return 1
+	}
+
+	// valid num: [1, n]
+	// use 1 to replace invalid num:
+	// 	- negative
+	// 	- num gt n
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] <= 0 || nums[i] > n {
+			nums[i] = 1
+		}
+	}
+
+	// use index as hash value
+	// traverse array
+	// make it dualistic:
+	// 	- get nums[i], make the index nums[i] negative only once
+	// 	- use index 0 to represent n exists
+	for i := 0; i < n; i++ {
+		idx := nums[i]
+		if idx == n {
+			idx = 0
+		}
+		if idx >= 0 && nums[idx] > 0 {
+			nums[idx] = -nums[idx]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			return i
+		}
+	}
+
+	return n + 1
 }
--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -392,3 +392,41 @@ func Test_majorityElementII(t *testing.T) {
 		})
 	}
 }
+
+func Test_firstMissingPositive(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "first missing positive",
+			args: args{
+				nums: []int{0, 1, 2},
+			},
+			want: 4,
+		}, {
+			name: "first missing positive",
+			args: args{
+				nums: []int{3, 4, -1, 1},
+			},
+			want: 2,
+		}, {
+			name: "first missing positive",
+			args: args{
+				nums: []int{7, 8, 9, 11, 12},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstMissingPositive(tt.args.nums); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
