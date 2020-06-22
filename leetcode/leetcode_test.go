6230ec84ba50fcc2f17b5614f8d3ca70370643a6
junyi <cai_junyi@yeah.net>
2020-06-22 18:43:42 +0800

    leetcode 674: longest continuous increasing subsequence

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -610,3 +610,41 @@ func Test_numIslands(t *testing.T) {
 		})
 	}
 }
+
+func Test_findLengthOfLCIS(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "longest continuous increasing subsequence",
+			args: args{
+				nums: []int{1, 3, 5, 7},
+			},
+			want: 4,
+		}, {
+			name: "longest continuous increasing subsequence",
+			args: args{
+				nums: []int{2, 2, 2, 2},
+			},
+			want: 1,
+		}, {
+			name: "longest continuous increasing subsequence",
+			args: args{
+				nums: []int{3, 0, 6, 2, 4, 7, 0, 0},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findLengthOfLCIS(tt.args.nums); got != tt.want {
+				t.Errorf("findLengthOfLCIS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
