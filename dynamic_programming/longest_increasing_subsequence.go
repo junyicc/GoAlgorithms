e34a172e9a071e00da2a6d5eff5dfb157c060799
junyi <cai_junyi@yeah.net>
2020-06-23 19:26:43 +0800

    longest increasing subsequence

--- dynamic_programming/longest_increasing_subsequence.go
+++ dynamic_programming/longest_increasing_subsequence.go
@@ -1,13 +1,4 @@
 package dynamic_programming
 
-func LongestIncreasingSubsequence(arr []int) []int {
-	if len(arr) < 1 {
-		return nil
-	}
-
-	return longestIncreasingSubsequence(arr)
-}
-
-func longestIncreasingSubsequence(arr []int) []int {
-	return nil
-}
+// see: leetcode 300 length of LIS
+// see: leetcode 673 number of LIS
