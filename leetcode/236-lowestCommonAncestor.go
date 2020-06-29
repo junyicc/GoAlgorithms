d002c414a257cbea865c9d7c3866d51329cba4fa
junyi <cai_junyi@yeah.net>
2020-06-29 10:16:50 +0800

    leetcode 236: lowest common ancestor

---
+++ leetcode/236-lowestCommonAncestor.go
@@ -0,0 +1,29 @@
+package leetcode
+
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+//
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	if right != nil {
+		return right
+	}
+	return nil
+}
