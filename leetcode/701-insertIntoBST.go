0faa3b750d7c9b5df385cbad879bd2e29e798679
junyi <cai_junyi@yeah.net>
2020-07-08 17:14:12 +0800

    leetcode 701: insert into bst

---
+++ leetcode/701-insertIntoBST.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	return root
+}
