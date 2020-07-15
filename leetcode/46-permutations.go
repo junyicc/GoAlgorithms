77c660a460e38d183eda6e5036d0f9efb67e68e0
junyi <cai_junyi@yeah.net>
2020-07-15 14:36:11 +0800

    leetcode 46: using backtrack to permute nums

--- leetcode/46-permutations.go
+++ leetcode/46-permutations.go
@@ -26,3 +26,46 @@ func arrPermutations(nums []int, start, n int, f func([]int)) {
 		nums[start], nums[i] = nums[i], nums[start]
 	}
 }
+
+func permuteWithBacktracking(nums []int) [][]int {
+	if len(nums) < 1 {
+		return nil
+	}
+	res := [][]int{}
+	f := func(nums []int) {
+		res = append(res, nums)
+	}
+	track := make([]int, 0, len(nums))
+	backtrackPermute(nums, track, f)
+
+	return res
+}
+
+func backtrackPermute(nums []int, track []int, f func([]int)) {
+	if len(track) == len(nums) {
+		clone := append(track[:0:0], track...)
+		f(clone)
+		return
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if inTrack(track, nums[i]) {
+			continue
+		}
+		// make choice
+		track = append(track, nums[i])
+
+		backtrackPermute(nums, track, f)
+
+		track = track[:len(track)-1]
+	}
+}
+
+func inTrack(track []int, num int) bool {
+	for _, n := range track {
+		if n == num {
+			return true
+		}
+	}
+	return false
+}
