1c8079336282be50c55fdf82b86cdcd357d2620e
junyi <cai_junyi@yeah.net>
2020-07-20 10:57:16 +0800

    leetcode 1011: min ship load

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -1880,3 +1880,31 @@ func Test_minEatingSpeed(t *testing.T) {
 		})
 	}
 }
+
+func Test_shipWithinDays(t *testing.T) {
+	type args struct {
+		weights []int
+		D       int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "can ship",
+			args: args{
+				weights: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				D:       5,
+			},
+			want: 15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shipWithinDays(tt.args.weights, tt.args.D); got != tt.want {
+				t.Errorf("shipWithinDays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
