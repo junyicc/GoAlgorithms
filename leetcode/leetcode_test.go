95ac3e8e7dab2ad0b5e2770ff9b9826d64624b76
junyi <cai_junyi@yeah.net>
2020-07-23 16:15:08 +0800

    leetcode 292: nim game

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -2052,3 +2052,29 @@ func Test_findMinArrowShots(t *testing.T) {
 		})
 	}
 }
+
+func Test_canWinNim(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "nim game",
+			args: args{
+				n: 6,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canWinNim(tt.args.n); got != tt.want {
+				t.Errorf("canWinNim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
