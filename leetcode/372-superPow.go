6dbc5b143dffdeb0d35f6d6c57588d40e76a5228
junyi <cai_junyi@yeah.net>
2020-07-25 11:47:16 +0800

    leetcode 372: super pow

---
+++ leetcode/372-superPow.go
@@ -0,0 +1,49 @@
+package leetcode
+
+// Your task is to calculate a^b mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+//
+// Example 1:
+// Input: a = 2, b = [3]
+// Output: 8
+//
+// Example 2:
+// Input: a = 2, b = [1,0]
+// Output: 1024
+
+func superPow(a int, b []int) int {
+	if len(b) < 1 {
+		return 1
+	}
+
+	base := 1337
+
+	// (a*b)%k = (a%k)(b%k)%k
+	mypow := func(a, k int) int {
+		// k is between [0, 10]
+		res := 1
+
+		// may overflow
+		// for i := 0; i < k; i++ {
+		// 	tmp := a % base
+		// 	res *= tmp
+		// }
+
+		tmp := a % base
+		for i := 0; i < k; i++ {
+			res *= tmp
+			res %= base
+		}
+
+		return res
+	}
+
+	// a^1234 = a^4 * a^123^10
+	last := b[len(b)-1]
+	b = b[:len(b)-1]
+
+	// part1 = ((a^last)%base) % base
+	part1 := mypow(a, last)
+	part2 := mypow(superPow(a, b), 10)
+
+	return part1 * part2 % base
+}
--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -2104,3 +2104,38 @@ func Test_lengthOfLongestSubstringWithSlidingWin(t *testing.T) {
 		})
 	}
 }
+
+func Test_superPow(t *testing.T) {
+	type args struct {
+		a int
+		b []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "super pow",
+			args: args{
+				a: 2147483647,
+				b: []int{2, 0, 0},
+			},
+			want: 1198,
+		}, {
+			name: "super pow",
+			args: args{
+				a: 2,
+				b: []int{1, 0},
+			},
+			want: 1024,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := superPow(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("superPow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
