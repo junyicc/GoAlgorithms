7bb8b7da95ad0884c3a2ca8a1ef07c1997162879
junyi <cai_junyi@yeah.net>
2020-07-25 12:56:09 +0800

    leetcode 372: upgrade pow function

--- leetcode/372-superPow.go
+++ leetcode/372-superPow.go
@@ -47,3 +47,40 @@ func superPow(a int, b []int) int {
 
 	return part1 * part2 % base
 }
+
+func superPow2(a int, b []int) int {
+	if len(b) < 1 {
+		return 1
+	}
+
+	base := 1337
+
+	// a^1234 = a^4 * a^123^10
+	last := b[len(b)-1]
+	b = b[:len(b)-1]
+
+	// part1 = ((a^last)%base) % base
+	part1 := mypow(a, last, base)
+	part2 := mypow(superPow(a, b), 10, base)
+
+	return part1 * part2 % base
+}
+
+func mypow(a, k int, base int) int {
+	if k == 0 {
+		return 1
+	}
+	if k == 1 {
+		return a % base
+	}
+
+	// (a*b)%k = (a%k)(b%k)%k
+	res := mypow(a, k>>1, base)
+	res *= res
+	// k is odd
+	if k&1 == 1 {
+		res *= a % base
+	}
+
+	return res % base
+}
--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -2133,7 +2133,7 @@ func Test_superPow(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := superPow(tt.args.a, tt.args.b); got != tt.want {
+			if got := superPow2(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("superPow() = %v, want %v", got, tt.want)
 			}
 		})
