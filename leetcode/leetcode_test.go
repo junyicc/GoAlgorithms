a613f695b498ac3cec5a752b8dfb059a888e32c7
junyi <cai_junyi@yeah.net>
2020-07-27 15:03:03 +0800

    leetcode 714: max profit with fee

--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -2139,3 +2139,31 @@ func Test_superPow(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxProfitWithFee(t *testing.T) {
+	type args struct {
+		prices []int
+		fee    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "max profit with fee",
+			args: args{
+				prices: []int{1, 3, 2, 8, 4, 9},
+				fee:    2,
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfitWithFee(tt.args.prices, tt.args.fee); got != tt.want {
+				t.Errorf("maxProfitWithFee() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
