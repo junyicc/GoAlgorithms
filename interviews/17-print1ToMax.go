c6d65db103b20097ea6e83ad0a6b13f026fc62c4
junyi <cai_junyi@yeah.net>
2020-08-06 18:10:42 +0800

    剑指offer 17: print 1 to max

--- interviews/17-print1ToMax.go
+++ interviews/17-print1ToMax.go
@@ -2,8 +2,27 @@ package interviews
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
+
+// 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+//
+// 示例 1:
+// 输入: n = 1
+// 输出: [1,2,3,4,5,6,7,8,9]
+
+func printNumbers(n int) []int {
+	if n < 1 {
+		return nil
+	}
+	cnt := int(math.Pow10(n))
+	res := make([]int, cnt-1)
+	for i := 1; i < cnt; i++ {
+		res[i-1] = i
+	}
+	return res
+}
 
 // Print1ToMaxOfNDigits prints 1 to max of n digits
 func Print1ToMaxOfNDigits(n int) {
